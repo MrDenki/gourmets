2e385982dfeda866a7c090c7a883d9ba0a8e95d5
ZiNgER-TW <zinger.tw@gmail.com>
2021-05-16 23:13:00 +0700

    Fix bags

--- static/script.js
+++ static/script.js
@@ -1,8 +1,33 @@
-var doc = document.select_ing_form.i1
-doc.addEventListener("click", writeData);
-function writeData(e) {
-    var data1 = document.select_ing_form.select_ing
-    var data2 = document.select_ing_form.write_ing
-    data1.value=324
-    data2.value = "Все херня"
+
+var search = document.si_form
+var textar = document.si_form.write_ing
+search.addEventListener("click", searchFunction)
+function searchFunction(e) {
+    var target1 = e.target
+    switch (target1.name) {
+        case "i1":
+            textar.value = "Молочные продукты";
+            break;
+        case "i2":
+            textar.value = "Крупы";
+            break;
+        case "i3":
+            textar.value = "Овощи";
+            break;
+        case "i4":
+            textar.value = "Специи";
+            break;
+        case "i5":
+            textar.value = "Мясо";
+            break;
+        case "i6":
+            textar.value = "Напитки";
+            break;
+        case "i7":
+            textar.value = "Макароны";
+            break;
+        case "i8":
+            textar.value = "Фрукты";
+            break;
+    }
 }
