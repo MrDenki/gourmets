816d774842c75afe3afc5f81bddbb34c1bfe962d
ZiNgER-TW <zinger.tw@gmail.com>
2021-05-25 10:26:34 +0700

    Fixed design

--- static/recipe_script.js
+++ static/recipe_script.js
@@ -1,20 +1,10 @@
 var form = document.form_test;
 var btn = form.sended;
+alert()
 btn.addEventListener("click", sendRequest);
 function sendRequest(e) {
     e.preventDefault();
-    var mes ={};
-    mes.nick = form.i1.value;
-    mes.text = form.i2.text;
-    var request = new XMLHttpRequest();
-    function reqReadyStateChange() {
-        if (request.readyState == 4 && request.status == 200){
-            alert("success!");
-        }
-        else{
-            alert("fail!" + request.readyState);
-        }
-
+    
     }
     var sends = "nick="+mes.nick + "text="+mes.text;
     request.open("POST","127.0.0.1:8000");
